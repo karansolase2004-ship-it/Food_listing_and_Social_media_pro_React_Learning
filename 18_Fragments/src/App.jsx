22eee91158cecd31b4da20b67c03468cefcc113d
karansolase <126546501+karansolase@users.noreply.github.com>
2026-09-01 16:10:27 +0530

    Fragments

---
+++ 18_Fragments/src/App.jsx
@@ -0,0 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import React from "react";
+
+function App() {
+
+  return ( 
+    <>
+      <div>Healthy Foods</div>
+      <ul class="list-group">
+        <li className="list-group-item">An Item</li>
+        <li className="list-group-item">A Second Item</li>
+        <li className="list-group-item">A third Item</li>
+        <li className="list-group-item">A fourth Item</li>
+        <li className="list-group-item">And a fifth Item</li>
+      </ul>
+    </>
+  );
+}
+
+export default App
